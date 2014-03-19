94f5dd6a4cbe5cb9f7d2351e9de7a0bf4ae7f576
Phil Hagelberg <technomancy@gmail.com>
2014-03-18 20:17:00 -0700

    Upgrade ring to work around uberjar resources bug.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
                  [org.clojure/java.jdbc "0.2.1"]
                  [postgresql "9.1-901-1.jdbc4"]
                  [hiccup "1.0.2"]
-                 [ring/ring-jetty-adapter "1.1.6"]]
+                 [ring/ring-jetty-adapter "1.2.2"]]
   :min-lein-version "2.0.0"
   :uberjar-name "lein-survey-standalone.jar"
   :main lein-survey.web
